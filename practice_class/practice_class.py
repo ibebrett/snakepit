b724874c37b37632096e8401b46412de1f161ace
Michael <welsh.11311@gmail.com>
2019-08-31 10:26:02 -0400

    Added greeting

--- practice_class/practice_class.py
+++ practice_class/practice_class.py
@@ -5,9 +5,15 @@ class Character:
 
     def speak(self):
         return f'I am {self.name} and I have {self.hp} hp'
+    
+    def wave(self):
+        return f'{self.name} waves hello.'
 
 mike = Character('mike', 10)
 sally = Character('sally', 12)
 
 print(sally.speak())
 print(sally.name)
+print(sally.wave())
+
+
