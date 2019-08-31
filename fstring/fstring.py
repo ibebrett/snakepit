af7d317d95aef64c011f4dce6dd1261cbf18df16
Brett Jurman <i.be.brett@gmail.com>
2019-08-31 09:45:13 -0400

    updating example

--- fstring/fstring.py
+++ fstring/fstring.py
@@ -2,5 +2,8 @@ x = 1
 y = 'hello'
 
 a_cool_string = f'using f strings is fun, {x} and {y}'
+not_cool_string = 'using f strings is fun, {x} and {y}'
 
+# NOTE how the f string actually uses the values of x and y.
 print(a_cool_string)
+print(not_cool_string)
