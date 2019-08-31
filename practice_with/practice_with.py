6053e55fa85c2a54ddc175bfa0388c2a5e4f1b57
Brett Jurman <i.be.brett@gmail.com>
2019-08-31 10:55:19 -0400

    adding timer

--- practice_with/practice_with.py
+++ practice_with/practice_with.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Battle:
     def __enter__(self):
         print("The battle is starting")
@@ -5,7 +8,23 @@ class Battle:
     def __exit__(self, *args):
         print("The battle is ending")
 
+
+class Timer:
+    def __enter__(self):
+        self.start_time = datetime.now()
+
+    def __exit__(self, *args):
+        total_time = datetime.now() - self.start_time
+        print(f"Timer took {total_time}")
+        
+
 battle = Battle()
 
 with battle:
     print("woohoo attack")
+
+
+with Timer():
+    for i in range(2, 10000):
+        for j in range(2, i-1):
+            pass
